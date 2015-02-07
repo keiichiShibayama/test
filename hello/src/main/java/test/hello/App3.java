259602d7ba02c10446c4cf501a98ab7c63a1cd46
keiichi shibayama <shibacchi.sk@gmail.com>
2015-02-08 00:07:20 +0900

    no message

---
+++ hello/src/main/java/test/hello/App3.java
@@ -0,0 +1,13 @@
+package test.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
